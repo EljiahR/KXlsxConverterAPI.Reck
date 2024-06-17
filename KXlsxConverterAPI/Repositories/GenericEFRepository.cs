96bcd1de4b1652320b201dc16728f780404dc878
Elijah Reck <kcerhajile@gmail.com>
2024-06-16 21:41:31 -0400

    Added a generic entity framework repository, I felt inclined to make it generic despite knowing realistically it will probably only use the Employee model

---
+++ KXlsxConverterAPI/Repositories/GenericEFRepository.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace KXlsxConverterAPI.Repositories;
+
+public class GenericEFRepository<TEntity, TContext> : IGenericEFRepository<TEntity>
+    where TEntity : class
+    where TContext : DbContext
+{
+    private readonly TContext _context;
+    private readonly DbSet<TEntity> _dbSet;
+    public GenericEFRepository(TContext context)
+    {
+        _context = context;
+        _dbSet = _context.Set<TEntity>();
+    }
+    public void Add(TEntity entity)
+    {
+        _dbSet.Add(entity);
+        SaveChanges();
+    }
+
+    public void Delete(TEntity entity)
+    {
+        _dbSet.Remove(entity);
+    }
+
+    public IEnumerable<TEntity> GetAll()
+    {
+        return _dbSet.ToList();
+    }
+
+    public TEntity GetById(int id)
+    {
+        return _dbSet.Find(id);
+    }
+
+    public void SaveChanges()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Update(TEntity entity)
+    {
+        _dbSet.Update(entity);
+        SaveChanges();
+    }
+}
--- KXlsxConverterAPI/Repositories/IGenericEFRepository.cs
+++ KXlsxConverterAPI/Repositories/IGenericEFRepository.cs
@@ -1,12 +1,11 @@
-﻿namespace KXlsxConverterAPI.Repositories
+﻿namespace KXlsxConverterAPI.Repositories;
+
+public interface IGenericEFRepository<TEntity>
 {
-    public interface IGenericEFRepository<T>
-    {
-        T GetById(int id);
-        IEnumerable<T> GetAll();
-        void Add(T entity);
-        void Delete(T entity);
-        void Update(T entity);
-        void SaveChanges();
-    }
+    TEntity GetById(int id);
+    IEnumerable<TEntity> GetAll();
+    void Add(TEntity entity);
+    void Delete(TEntity entity);
+    void Update(TEntity entity);
+    void SaveChanges();
 }
