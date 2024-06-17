8c87079bb928c0520611f33cc15449808309cf63
Elijah Reck <kcerhajile@gmail.com>
2024-06-16 21:51:37 -0400

    Created interface for future EmployeeService

---
+++ KXlsxConverterAPI/Services/IEmployeeService.cs
@@ -0,0 +1,13 @@
+﻿using KXlsxConverterAPI.Models;
+
+namespace KXlsxConverterAPI.Services
+{
+    public interface IEmployeeService
+    {
+        IEnumerable<Employee> GetAllEmployees();
+        Employee GetEmployeeById(int id);
+        void AddEmployee(Employee employee);
+        void DeleteEmployee(Employee employee);
+        void UpdateEmployee(Employee employee);
+    }
+}
