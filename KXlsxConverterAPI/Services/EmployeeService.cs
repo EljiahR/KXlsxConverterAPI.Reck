449679310fbd9c062b5c57eac00bfbbf554f2c6b
Elijah Reck <kcerhajile@gmail.com>
2024-06-16 21:54:45 -0400

    Created EmployeeService wtih repo injection, yet to emplement methods

---
+++ KXlsxConverterAPI/Services/EmployeeService.cs
@@ -0,0 +1,39 @@
+﻿using KXlsxConverterAPI.Models;
+using KXlsxConverterAPI.Repositories;
+
+namespace KXlsxConverterAPI.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IGenericEFRepository<Employee> _repository;
+        public EmployeeService(IGenericEFRepository<Employee> repository)
+        {
+            _repository = repository;
+        }
+
+        public void AddEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Employee GetEmployeeById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateEmployee(Employee employee)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
