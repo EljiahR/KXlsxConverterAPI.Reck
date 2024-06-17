5f3d5b83d3b4ced692722c828a8e0a822f1d5e07
Elijah Reck <kcerhajile@gmail.com>
2024-06-16 21:46:53 -0400

    Added EmployeesContext

---
+++ KXlsxConverterAPI/Data/EmployeeContext.cs
@@ -0,0 +1,15 @@
+﻿using KXlsxConverterAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace KXlsxConverterAPI.Data
+{
+    public class EmployeeContext : DbContext
+    {
+        public EmployeeContext(DbContextOptions<EmployeeContext> options) : base(options)
+        { 
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
