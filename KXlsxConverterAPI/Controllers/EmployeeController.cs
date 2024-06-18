07e638addf92ae95e45f7c2826b847f73655f7a5
Elijah Reck <kcerhajile@gmail.com>
2024-06-18 12:29:50 -0400

    Craeted EmployeeController with GetAll and Post

---
+++ KXlsxConverterAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,40 @@
+﻿using KXlsxConverterAPI.Models;
+using KXlsxConverterAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KXlsxConverterAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class EmployeeController : Controller
+{
+    private readonly IEmployeeService _service;
+    public EmployeeController(IEmployeeService service)
+    {
+        _service = service;
+    }
+    [HttpGet]
+    public IActionResult ViewAllEmployees()
+    {
+        var employees = _service.GetAllEmployees();
+        if (employees == null)
+        {
+            return NotFound();
+        }
+        return Ok(employees);
+    }
+    [HttpPost]
+    public IActionResult PostEmployee(Employee employee)
+    {
+        try
+        {
+            _service.AddEmployee(employee);
+            return Created();
+        }
+        catch (Exception ex)
+        {
+            return BadRequest();
+        }
+
+    }
+}
