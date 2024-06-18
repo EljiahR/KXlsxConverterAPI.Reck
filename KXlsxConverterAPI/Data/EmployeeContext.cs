1c99e6f1acaff0558cea09b1640a115ffb3228e6
Elijah Reck <kcerhajile@gmail.com>
2024-06-18 13:06:02 -0400

    Removed entity properties from EmployeeContext and changed Employee model to replace those properties

--- KXlsxConverterAPI/Data/EmployeeContext.cs
+++ KXlsxConverterAPI/Data/EmployeeContext.cs
@@ -13,29 +13,5 @@ namespace KXlsxConverterAPI.Data
         }
 
         public DbSet<Employee> Employees { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            // Honestly couldn't find anything on setting multiple properties so I apologize if theres a better way
-            // Setting nullable fields
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.PreferredFirstName).IsRequired(false);
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.Birthday).IsRequired(false);
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.PositionOverride).IsRequired(false);
-            // Setting defaults
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.PreferredNumberOfBreaks)
-                .HasDefaultValue(2);
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.GetsLunchAsAdult)
-                .HasDefaultValue(false);
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.BathroomOrder)
-                .HasDefaultValue(0);
-            modelBuilder.Entity<Employee>()
-                .Property(e => e.IsACallUp)
-                .HasDefaultValue(true);
-        }
     }
 }
--- KXlsxConverterAPI/Models/Employee.cs
+++ KXlsxConverterAPI/Models/Employee.cs
@@ -4,20 +4,20 @@ public class Employee
 {
     public int EmployeeId { get; set; } // Not based on any actual corporate based IDs
     public string FirstName { get; set; } // Name registered in MyTime 
-    public string PreferredFirstName { get; set; } // Name employee prefers if there is one
+    public string? PreferredFirstName { get; set; } // Name employee prefers if there is one
     public string LastName { get; set; } // Don't make me explain it
-    public DateTime Birthday { get; set; } // Only Year, Month, and Day necessary
-    public int PreferredNumberOfBreaks { get; set; } // Preferred number of break if working >= 6 hours, should default to 2
+    public DateTime? Birthday { get; set; } // Only Year, Month, and Day necessary
+    public int PreferredNumberOfBreaks { get; set; } = 2; // Preferred number of break if working >= 6 hours, should default to 2
 
     // Minors get a mandatory 30 minute unpaid lunch when working >= 6 hours, deducts from shift length
-    public bool GetsLunchAsAdult { get; set; }
+    public bool GetsLunchAsAdult { get; set; } = false;
 
     // Should be null more often than not and used sparingly, mostly for fixes small bugs/errors caused by the schedule, can use DELETE 
-    public string PositionOverride { get; set; }
+    public string? PositionOverride { get; set; }
 
     // Should default to 0 unless a bagger and trained to clean bathrooms in the morning, should go in order of 1 scheduled 1st, 2 2nd, etc...
-    public int BathroomOrder { get; set; }
+    public int BathroomOrder { get; set; } = 0;
 
     // For employees either not in front-end and can be called up or front-end employees that are occasionally scheduled in different departments
-    public bool IsACallUp { get; set; }
+    public bool IsACallUp { get; set; } = true;
 }
