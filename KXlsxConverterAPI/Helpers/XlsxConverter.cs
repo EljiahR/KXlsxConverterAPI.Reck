2a46c0464d82303efe80312c92552d73a6bda4cf
Elijah Reck <kcerhajile@gmail.com>
2024-06-21 21:04:09 -0400

    Seperated row identification into seperate method

--- KXlsxConverterAPI/Helpers/XlsxConverter.cs
+++ KXlsxConverterAPI/Helpers/XlsxConverter.cs
@@ -7,16 +7,26 @@ namespace KXlsxConverterAPI.Helpers
 {
     public class XlsxConverter
     {
+        private enum CurrentSection { FoundNewDay, FoundEmployee, FoundEnd, FoundNothing };
         private static Regex dayOfWeekRegex = new Regex(@"^[A-Z]{1}[a-z]{2}\s\d{2}\/\d{2}\/\d{4}$");
         private static Regex nameRegex = new Regex(@"^[a-zA-Z]+,\s[a-zA-Z]+");
         
+        private List<WeekdaySchedule> days;
+        private WeekdaySchedule? currentDay;
+        private ExcelWorksheet? ws;
+
         private static int nameColumnPos = 2;
-        public static List<WeekdaySchedule> ConvertXlsx(Stream stream, IEnumerable<Employee> storeEmployees)
+
+        public XlsxConverter()
         {
-            List<WeekdaySchedule> days = new List<WeekdaySchedule>();
+            days = new();
+        }
+        public List<WeekdaySchedule> ConvertXlsx(Stream stream, IEnumerable<Employee> storeEmployees)
+        {
+            
             using (ExcelPackage package = new ExcelPackage(stream))
             { 
-                ExcelWorksheet ws = package.Workbook.Worksheets[0];
+                ws = package.Workbook.Worksheets[0];
                 if(ws == null)
                 {
                     throw new NullReferenceException("Worksheet is empty");
@@ -25,35 +35,44 @@ namespace KXlsxConverterAPI.Helpers
                 int colCount = ws.Dimension.Columns;
 
                 bool dayFound = false; // For tracking when in a valid range for shifts
+                WeekdaySchedule? currentDay = null;
                 for (int row = 1; row <= rowCount; row++)
                 {
-                    // First cell in row contains information regarding either current day or "Forcasted" which indicates end of day
-                    string? firstCell = ws.Cells[row, 1].Value?.ToString();
-                    if (!dayFound && !string.IsNullOrEmpty(firstCell) && dayOfWeekRegex.IsMatch(firstCell))
+                    switch(IdentifyRow(row, dayFound))
                     {
-                        dayFound = true;
-                        DateTime newWeekday = DateTime.Parse(firstCell);
-                        days.Add(new WeekdaySchedule(newWeekday.ToString("dddd"), newWeekday));
-                    } else if(dayFound && firstCell == "Forcasted")
-                    {
-                        dayFound = false;
-                    }
-                    if (dayFound)
-                    {
-                        string? nameColumnValue = ws.Cells[row, nameColumnPos].Value?.ToString();
-                        if(!string.IsNullOrEmpty(nameColumnValue) && nameRegex.IsMatch(nameColumnValue))
-                        {
-
-                        }
+                        case CurrentSection.FoundNewDay:
+                            dayFound = true;
+                            DateTime newWeekday = DateTime.Parse(ws.Cells[row, 1].Value?.ToString() ?? "");
+                            currentDay = new WeekdaySchedule(newWeekday.ToString("dddd"), newWeekday);
+                            days.Add(currentDay);
+                            break;
+                        case CurrentSection.FoundEmployee when dayFound:
+                            break;
+                        case CurrentSection.FoundEnd:
+                            dayFound = false;
+                            currentDay = null;
+                            break;
                     }
                 }
             }
             
             
             
-            days.Add(new WeekdaySchedule("Testday", DateTime.Today));
+            //days.Add(new WeekdaySchedule("Testday", DateTime.Today));
 
             return days;
+        }
+
+        private CurrentSection IdentifyRow(int row, bool dayFound)
+        {
+            
+            if (dayOfWeekRegex.IsMatch(ws.Cells[row, 1].Value?.ToString() ?? ""))
+                return CurrentSection.FoundNewDay;
+            else if (dayFound && ws.Cells[row, 1].Value?.ToString() == "Forcasted")
+                return CurrentSection.FoundEnd;
+            else if (dayFound && nameRegex.IsMatch(ws.Cells[row, nameColumnPos].Value?.ToString() ?? ""))
+                return CurrentSection.FoundEmployee;
+            return CurrentSection.FoundNothing;
         }
     }
 }
