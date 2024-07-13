46d76250d20d14fb8522e202adaaf57448a49c7f
Elijah Reck <kcerhajile@gmail.com>
2024-07-13 12:35:32 -0400

    Minor changes from Azure publish

--- KXlsxConverterAPI/Program.cs
+++ KXlsxConverterAPI/Program.cs
@@ -6,9 +6,15 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 var services = builder.Services;
 
+string? dbConnection = builder.Configuration["POSTGRESQLCONNSTR_DatabaseConnectionString"];
+if(string.IsNullOrEmpty(dbConnection)) dbConnection = builder.Configuration["DatabaseConnectionString"];
 // Add services to the container.
+
+//services.AddDbContext<EmployeeContext>(options =>
+//    options.UseSqlite("Data Source=employees.db"), ServiceLifetime.Scoped);
+
 services.AddDbContext<EmployeeContext>(options =>
-    options.UseSqlite("Data Source=employees.db"), ServiceLifetime.Scoped);
+    options.UseNpgsql(dbConnection));
 
 services.AddScoped<IEmployeeRepository, EmployeeRepository>();
 services.AddScoped<IEmployeeService, EmployeeService>();
