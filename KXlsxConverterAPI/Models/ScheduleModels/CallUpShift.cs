1a9aa151eb1d4a5f68c8b28d6f171a1c50557a26
Elijah <kcerhajile@gmail.com>
2024-07-19 16:09:03 -0400

    Added interface for shift and a new CallUpShift class that also uses this interface, added Errors property to Weekday class

--- KXlsxConverterAPI/Models/ScheduleModels/CallUpShift.cs
+++ KXlsxConverterAPI/Models/ScheduleModels/CallUpShift.cs
@@ -2,16 +2,16 @@
 
 namespace KXlsxConverterAPI.Models.ScheduleModels
 {
-    public class Shift : IShift
+    public class CallUpShift : IShift
     {
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string BaggerName {  get; set; } = string.Empty;
+        public string BaggerName { get; set; } = string.Empty;
         public DateTime ShiftStart { get; set; }
         public DateTime ShiftEnd { get; set; }
         public DateTime? BreakOne { get; set; } = null;
         public DateTime? Lunch { get; set; } = null;
         public DateTime? BreakTwo { get; set; } = null;
-
+        public string OriginalPosition {  get; set; } = string.Empty;
     }
 }
