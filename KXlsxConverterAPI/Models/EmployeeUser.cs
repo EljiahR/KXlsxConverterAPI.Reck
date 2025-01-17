e601dd4bb54e297a5a97a4c3e68372f7591c54b9
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-01-17 21:02:28 +0000

    Changed UserLogin model to EmployeeUser extendeing from IdentityUser

---
+++ KXlsxConverterAPI/Models/EmployeeUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace KXlsxConverterAPI.Models
+{
+    public class EmployeeUser : IdentityUser
+    {
+        public override string? UserName { get; set; }
+        public override string? Email { get; set; }
+    }
+}
