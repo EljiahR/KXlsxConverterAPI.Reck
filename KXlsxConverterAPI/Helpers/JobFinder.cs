644febf2cbe1fdbf731bcfbfd597c996a4deb0d6
Elijah Reck <142916290+EljiahR@users.noreply.github.com>
2025-02-09 13:26:02 +0000

    Improved how subKeys are tracked

--- KXlsxConverterAPI/Helpers/JobFinder.cs
+++ KXlsxConverterAPI/Helpers/JobFinder.cs
@@ -10,14 +10,14 @@
             {"P", "Front End Supervisor" },
             {"U", "Front End SCO Cashier" },
             {"$", "Front End Cashier" },
-            {"B", "Front End Courtesy Clerk" },
-            
+            {"B", "Front End Courtesy Clerk" },  
             {"!", "Front End Service Desk" },
             
             // Common jobs for front end employees to have a split shift with
             {"Z", "Fuel Clerk" },
             {"L", "Liquor Clerk" },
             {"W", "Liquor Clerk" }, // Actually Wine/Beer Steward but thats not necessary to know
+            
             // Job keys that are typically used as call ups for front end
             {"C", "HBC Clerk" },
             {"M", "GM MrktPlc Clk" }, // Not sure why so heavily shortened
@@ -27,13 +27,19 @@
         };
 
         // For tracking sub jobs that are still in the parent section but needed for other purposes
+        // Structured as SubJob's key ->
         // i.e. Utility clerks should not be scheduled for carts
-        public static readonly Dictionary<string, Dictionary<string, string>> subJobKeys = new()
+        public static readonly Dictionary<string, SubJobKeyDescription> subJobKeys = new()
         {
-            {"B", new Dictionary<string, string>{
-                {"/", "Front End Utitity Clerk"} // Utility clerk
-            }}, 
+            {"/", new SubJobKeyDescription("B", "Front End Utility Clerk")}, 
         };
+
+        public class SubJobKeyDescription(string parentKey, string title)
+        {
+            public string ParentKey = parentKey;
+            public string Title = title;
+
+        }
 
         // Random characters that need to be ignored in any given employee row to help find real split shifts
         // The ~ and ^ are breaks and lunches I think. The rest are trash icons typically from bad reports which can sometimes be fixed by re-running the report. 
